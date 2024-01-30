88fbed01d62f63c22717cead3f81f565a570195f
Чупахин Юрий <chupahin23@mail.ru>
2024-01-30 09:06:03 +0500

    Добавил комменты в код

--- dag.py
+++ dag.py
@@ -1,6 +1,8 @@
 import sys
 
-def start_text_dag_func(owner,schedule_interval, start_date, retries,retry_delay,description,nameDag):
+
+def start_text_dag_func(owner, schedule_interval, start_date, retries, retry_delay, description, nameDag):
+    '''Создаем начало файла и заполняем конфиги'''
     text_start = f'''
 from airflow import DAG
 from datetime import datetime, timedelta
@@ -23,7 +25,10 @@ dag = DAG(
 )
 '''
     return text_start
-def text_func(name_con,con_id,airbyte_conn_id):
+
+
+def text_func(name_con, con_id, airbyte_conn_id):
+    """Шаблон для Создания оператора для каждого Connections из Airbyte"""
     text_func = f'''
 start_airbyte{name_con} = AirbyteTriggerSyncOperator(
          task_id = "{name_con}",
@@ -37,35 +42,43 @@ start_airbyte{name_con} = AirbyteTriggerSyncOperator(
     '''
     return text_func
 
+
 def create_end_dags(list_):
-    return '\n'+('>>').join(list_).strip()
+    """Создаем последовательность запуска всех COnnerctions Airbyte"""
+    return '\n' + '>>'.join(list_).strip()
+
 
 def read_conf():
+    """Читаем конфиги"""
     with open('for_dag.txt', encoding='utf-8') as file:
         rows = file.readlines()
     return rows
 
+
 def create_python_file(path_folder_airflow):
+    """Основная функция
+    Создаем и заполняем файл .py
+    """
 
     rows = read_conf()
-    list_name_conn = []
+    list_name_conn = []  # Переменная для использования последовательного запуска операторов
 
-    with open(f'{path_folder_airflow}','w',encoding='utf-8') as file_dag:
+    with open(f'{path_folder_airflow}', 'w', encoding='utf-8') as file_dag:
 
-        default_args = {i.split(':')[0]:i.split(':')[1].replace('\n','').strip() for i in rows if len(i)>4}
+        default_args = {i.split(':')[0]: i.split(':')[1].replace('\n', '').strip() for i in rows if
+                        len(i) > 4}  # Словарь с конфигами
 
         start_text_dag = start_text_dag_func(
-                       owner=default_args['owner'],
-                       start_date=default_args['start_date'],
-                       retries=default_args['retries'],
-                       schedule_interval = default_args['schedule_interval'],
-                       retry_delay=default_args['retry_delay'],
-                       description=default_args['description'],
-                       nameDag=default_args['nameDag']
+            owner=default_args['owner'],
+            start_date=default_args['start_date'],
+            retries=default_args['retries'],
+            schedule_interval=default_args['schedule_interval'],
+            retry_delay=default_args['retry_delay'],
+            description=default_args['description'],
+            nameDag=default_args['nameDag']
         )
         file_dag.write(start_text_dag)
-
-
+#Ищем только ID Connections Airbyte для создания операторов
         for row in default_args:
             if 'name_' in row:
                 list_name_conn.append(f'start_airbyte{row}')
@@ -74,12 +87,9 @@ def create_python_file(path_folder_airflow):
                                                  con_id=con_id,
                                                  airbyte_conn_id=default_args['airbyte_conn_id'])
                 file_dag.write(text_func_connection)
-        file_dag.write(create_end_dags(list_name_conn).replace('  ',''))
+        file_dag.write(create_end_dags(list_name_conn).replace('  ', ''))
 
-
-
-
+#При запуске передаем путь и название новго файла (Файл должен быть сохранен в Airflow/dags/)
 if __name__ == "__main__":
     folder_airflow = str(sys.argv[1])
     create_python_file(path_folder_airflow=folder_airflow)
-
