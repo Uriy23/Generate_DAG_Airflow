0d6213ff23278de0504213d2e0d7fc4f41c64b4c
Чупахин Юрий <chupahin23@mail.ru>
2024-01-30 13:07:21 +0500

    Добавил уведомления в тг при ошибке в Dag

--- Generate_dag/Generate_dag.py
+++ Generate_dag/Generate_dag.py
@@ -1,13 +1,39 @@
 import sys
 
+def get_tokens_tg():
+    with open('Api_tg.txt', encoding='utf-8') as file:
+        rows = file.readlines()
+        API_tg = {i.split(':')[0]: i.split(':')[1].replace('\n', '').strip() for i in rows if len(i)>3}
+    return API_tg
+
+def create_allert_on_tg(tokens):
+
+    text_allert = f'''
+def on_failure_callback(context):    
+    message = "Task failed. DAG: {{0}}, Task: {{1}}, Execution date: {{2}}".format(
+        context['task_instance'].dag_id, 
+        context['task_instance'].task_id,
+        context['execution_date'])
+    for chat_id_person in {tokens['chat_id'].split(' ')}:
+        telegram_op = TelegramOperator(task_id='send_telegram',
+                                   token="{tokens['token'].replace('|',':')}", 
+                                   chat_id=chat_id_person,
+                                   text=message)
+        telegram_op.execute(context=context)'''
+    return text_allert
+
+
 
 def start_text_dag_func(owner, schedule_interval, start_date, retries, retry_delay, description, nameDag):
     '''Создаем начало файла и заполняем конфиги'''
+    global create_allert_on_tg,get_tokens_tg
     text_start = f'''
 from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.providers.airbyte.operators.airbyte import AirbyteTriggerSyncOperator
+from airflow.providers.telegram.operators.telegram import TelegramOperator
 
+{create_allert_on_tg(get_tokens_tg())}
 
 default_args = {{
     'owner': "{owner}",
@@ -21,14 +47,15 @@ dag = DAG(
     "{nameDag}",
     default_args=default_args,
     description="{description}",
-    schedule_interval="{schedule_interval}"
+    schedule_interval="{schedule_interval}",
+    on_failure_callback=on_failure_callback
 )
 '''
     return text_start
 
 
-def text_func(name_con, con_id, airbyte_conn_id):
-    """Шаблон для Создания оператора для каждого Connections из Airbyte"""
+def Airbyte_PP(name_con, con_id, airbyte_conn_id):
+    """Шаблон для Создания оператора для каждого Connections из Airbyte Postgres-Postgres"""
     text_func = f'''
 start_airbyte{name_con} = AirbyteTriggerSyncOperator(
          task_id = "{name_con}",
@@ -53,6 +80,15 @@ def read_conf():
     with open('Config_generate_dag.txt', encoding='utf-8') as file:
         rows = file.readlines()
     return rows
+
+
+def DBT_start_code():
+    # 1. git clone
+#     2. Запуск файла start_dbt.py
+    pass
+def Airbyte_PCH():
+    """Шаблон для Создания оператора для каждого Connections из Airbyte Postgres-CH"""
+    pass
 
 
 def create_python_file(path_folder_airflow):
@@ -83,7 +119,7 @@ def create_python_file(path_folder_airflow):
             if 'name_' in row:
                 list_name_conn.append(f'start_airbyte{row}')
                 con_id = default_args[row]
-                text_func_connection = text_func(name_con=row,
+                text_func_connection = Airbyte_code(name_con=row,
                                                  con_id=con_id,
                                                  airbyte_conn_id=default_args['airbyte_conn_id'])
                 file_dag.write(text_func_connection)
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Pipeline с помощью Airflow 
 
+## Создание оповещений в ТГ от Airflow 
+chat_id - можно узнать по ссылке https://api.telegram.org/bot<YourBOTToken>/getUpdates
+   
